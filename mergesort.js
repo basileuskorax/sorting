1e40653f79948641ed3d584cb7868c59c332198a
Evangelos Vekios <evekios@gmail.com>
2018-09-17 14:04:43 -0400

    setup

---
+++ mergesort.js
@@ -0,0 +1,11 @@
+function split(wholeArray) {
+  return [firstHalf, secondHalf];
+}
+
+function merge(arr1, arr2) {
+  /* your code here */
+}
+
+function mergeSort(array) {
+  /* your code here */
+}
---
+++ mergesort.spec.js
@@ -0,0 +1,17 @@
+describe("Split Array function", function() {
+  it("is able to split an array into two halves", function() {
+    // your code here
+  });
+});
+
+describe("Merge function", function() {
+  it("is able to merge two sorted arrays into one sorted array", function() {
+    // test the merging algorithm
+  });
+});
+
+describe("Merge sort", function() {
+  it("recursively runs merge sort", function() {
+    // test the merging algorithm
+  });
+});
